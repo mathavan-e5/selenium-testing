8d8f0d01eacf6ec6c549dfebcceb57aa0964ad29
Mathavan M <104998416+volthatter@users.noreply.github.com>
2022-05-13 00:16:15 +0530

    Add files via upload

---
+++ TwoWheel.java
@@ -0,0 +1,21 @@
+
+public class TwoWheel {
+	int noofWheel=2;
+	short noofMirror=2;
+	long chassisNumber=1347658759;
+	boolean ispunchured=true;
+	String bikeName="pulsar";
+	double running=1999.1;
+	public static void main(String[] args) {
+		TwoWheel object= new TwoWheel();
+		System.out.println(object.noofWheel);
+		System.out.println(object.noofMirror);
+		System.out.println(object.chassisNumber);
+		System.out.println(object.ispunchured);
+		System.out.println(object.bikeName);
+		System.out.println(object.running);
+		
+		
+	}
+
+}
